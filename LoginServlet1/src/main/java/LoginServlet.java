333cf4c83bdd32653f3db00a1fb0cb112cf05cbe
HP <HP@HarshKavitkar>
2022-08-07 20:52:51 +0530

    This is first commit

---
+++ LoginServlet1/src/main/java/LoginServlet.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+@WebServlet("/login")
+public class LoginServlet extends GenericServlet {
+	public void service(ServletRequest req, ServletResponse res) {
+		try {
+			String s1 = req.getParameter("uname");
+			String s2 = req.getParameter("pwd");
+			PrintWriter pw = res.getWriter();
+			
+			pw.println("<html><body bgcolor=yellow text=red><h1>");
+			if((s1.equals("abc"))&&(s2.equals("xyz"))){
+				pw.println("Welcome "+s1);
+			}
+			else {
+				pw.println("Invalid Uname/Pwd");
+			}
+			pw.println("</h1></body></html>");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
